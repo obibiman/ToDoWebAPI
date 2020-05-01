616412be1fd14059af11181c53232e31c7f23bd9
Maurice Muoneke <obibiman@yahoo.com>
2020-05-01 15:13:24 -0500

    Initial upload to GitHub
    This is the initial load of data from the web api project

--- ToDo.WebAPI.Tests/UnitTest1.cs
+++ ToDo.WebAPI.Tests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using System;
 using ToDo.Domain.Models;
@@ -8,23 +9,6 @@ namespace ToDo.WebAPI.Tests
 {
     public class TodoContestTest
     {
-        [Fact]
-        public void ShouleBoAbleToAddInMemoryDatabase()
-        {
-            var guid = Guid.NewGuid();
-            var builder = new DbContextOptionsBuilder<TodoContext>()
-                .EnableSensitiveDataLogging()
-                .UseInMemoryDatabase(guid.ToString());
-
-            using (var ctxt = new TodoContext(builder.Options))
-            {
-                ctxt.TodoItems.Add(new TodoItem() { Id = 1, Name = "Joseph Yomego", IsComplete = true, Secret = "Top secret" });
-                ctxt.TodoItems.Add(new TodoItem() { Id = 2, Name = "Mary Masjid", IsComplete = true, Secret = "Internal use only" });
-                ctxt.SaveChanges();
-
-                Assert.Equal(2, ctxt.TodoItems.Count(p=>p.Name = "Joseph Yomego"));
-            }
-
-        }
+   
     }
 }
